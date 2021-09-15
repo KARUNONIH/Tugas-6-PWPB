e4fb977a55c15ede08b42d07302b958d02dcd7a5
KARUNONIH <89448748+KARUNONIH@users.noreply.github.com>
2021-09-15 22:25:03 +0700

    Add files via upload

---
+++ Tugas 6/6.2.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Muhammad Gibran Anggalana</title>
+        <link rel="stylesheet" type="text/css" href="gaya.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap" rel="stylesheet"> 
+    </head>
+    <body>
+        <div class="overlay"></div>
+        <form action="6.2.php" method="post" class="box">
+            <div class="header">
+                <h3>Login to your account</h3>
+                <p>Use the account you have created to enter the website</p>
+            </div>
+            <div class="login-area">
+                <input type="text" class="username" placeholder="Username">
+                <input type="password" class="password" placeholder="Password">
+                <input type="submit" value="LOG IN" class="submit">
+            </div>
+        </form>
+    </body>
+</html>
